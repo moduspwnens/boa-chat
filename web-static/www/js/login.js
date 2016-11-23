7b56b2725d8d85460902afa1b298a8de80d3f535
Benn Linger <benn@linger.com>
2016-11-22 22:22:47 -0500

    Progress on login view front end.

--- web-static/www/js/login.js
+++ web-static/www/js/login.js
@@ -1,6 +1,52 @@
 'use strict';
 
 app.controller('loginController', function($scope, $http, $state, webchatService) {
-  $scope.title = "Log In";
   
+  $scope.formMode = "login";
+  
+  $scope.mainFormSubmitted = function() {
+    if ( $scope.formMode == "login" ) {
+      console.log("log in");
+      console.log($scope.email);
+      console.log($scope.password);
+    }
+    else {
+      console.log("create account");
+      console.log($scope.email);
+      console.log($scope.password);
+      console.log($scope.passwordConfirm);
+    }
+  }
+  
+  var updateFormSettingsToMatchFormMode = function() {
+    if ($scope.formMode == "login") {
+      $scope.title = "Log In";
+      $scope.mainActionButtonTitle = "Log in";
+      $scope.secondaryButtonTitle = "Create account";
+    }
+    else {
+      $scope.title = "Create account";
+      $scope.mainActionButtonTitle = "Create account";
+      $scope.secondaryButtonTitle = "Back to log in";
+    }
+  }
+  
+  var focusEmailField = function() {
+    window.setTimeout(function() {
+      document.getElementById("inputEmail").focus();
+    }, 0);
+  }
+  
+  $scope.switchFormTypeButtonClicked = function() {
+    if ($scope.formMode == "login") {
+      $scope.formMode = "create";
+    }
+    else {
+      $scope.formMode = "login";
+    }
+    updateFormSettingsToMatchFormMode();
+    focusEmailField();
+  }
+  
+  updateFormSettingsToMatchFormMode();
 });
